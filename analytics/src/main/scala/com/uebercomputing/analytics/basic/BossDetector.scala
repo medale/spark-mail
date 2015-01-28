7481a4fa56592e7df0cfeda9859f80640e0e4584
jschmidt10 <jschmidt10@github.com>
2015-01-28 10:05:46 -0500

    added a basic boss-detection analytic

---
+++ analytics/src/main/scala/com/uebercomputing/analytics/basic/BossDetector.scala
@@ -0,0 +1,76 @@
+package com.uebercomputing.analytics.basic
+
+import com.uebercomputing.mailrecord.MailRecordAnalytic
+import org.apache.log4j.Logger
+import scala.collection.JavaConversions._
+import org.apache.spark.SparkContext._
+import com.uebercomputing.mailrecord.Implicits._
+import com.uebercomputing.mailrecord.MailRecordOps
+
+/**
+ * Boss detector. Finds the users who have the lowest ratio of (cc) / (received).
+ * The feeling being that bosses are CC'd way more than they directly receive email.
+ *
+ * Required args:
+ *
+ * --avroMailInput AVRO_FILE --master local[*]
+ */
+object BossDetector extends MailRecordAnalytic {
+  val LOGGER = Logger.getLogger(BossDetector.getClass)
+
+  // how many should we see
+  val n = 10
+
+  def main(args: Array[String]) {
+    val appName = "BossDetector"
+    val additionalSparkProps = Map[String, String]()
+    val analyticInput = getAnalyticInput(appName, args, additionalSparkProps, LOGGER)
+
+    val addressTuples = analyticInput.mailRecordRdd.flatMap { mailRecord =>
+      val mro: MailRecordOps = mailRecord
+      val toList = mro.getToOpt().getOrElse(Nil)
+      val ccList = mro.getCcOpt().getOrElse(Nil)
+
+      (mailRecord.getFrom, "FROM") ::
+        toList
+        .map(addr => (addr, "TO")) ++
+        ccList
+        .map(addr => (addr, "CC"))
+    }
+
+    val fieldCounts = addressTuples
+      .groupByKey
+      .map {
+        case (addr, fields) =>
+          val counts = fields.groupBy(x => x).map(tuple => (tuple._1, tuple._2.size))
+          (addr, counts)
+      }
+
+    val topN = fieldCounts
+      .sortBy { case (addr, counts) => -BossScore(counts) }
+      .take(n)
+
+    topN
+      .foreach(tuple => println(tuple))
+  }
+}
+
+/**
+ * Toggle this to experiment with our boss detection.
+ */
+object BossScore extends Serializable {
+
+  val zero = 0.01
+  
+  /**
+   * Takes a map of field counts (TO, FROM, CC) and computes the boss score.
+   *
+   * @return the computed score
+   */
+  def apply(fieldCounts: Map[String, Int]): Double = {
+    val cc = fieldCounts.get("CC").map(_.toDouble).getOrElse(zero)
+    val to = fieldCounts.get("TO").map(_.toDouble).getOrElse(zero)
+    cc / to
+  }
+}
+
