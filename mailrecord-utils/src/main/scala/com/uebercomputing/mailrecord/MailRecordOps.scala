0e527eec4448fc316ec85a7855bbcf9ee61a7d71
medale <medale94@yahoo.com>
2015-02-02 19:36:27 -0500

    Used more idiomatic Scala processing

--- mailrecord-utils/src/main/scala/com/uebercomputing/mailrecord/MailRecordOps.scala
+++ mailrecord-utils/src/main/scala/com/uebercomputing/mailrecord/MailRecordOps.scala
@@ -1,53 +1,50 @@
 package com.uebercomputing.mailrecord
 
 import scala.collection.JavaConverters._
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
 
 case class MailRecordOps(mailRecord: MailRecord) {
 
   def getAttachmentsOpt(): Option[List[Attachment]] = {
     val attachments = mailRecord.getAttachments
-    if (attachments != null) {
-      val scalaList = attachments.asScala.toList
-      Some(scalaList)
-    } else {
-      None
-    }
+    getAsScalaListOption(attachments)
   }
 
   def getToOpt(): Option[List[String]] = {
-    val tos = mailRecord.getTo
-    if (tos != null) {
-      val scalaList = tos.asScala.toList
-      Some(scalaList)
-    } else {
-      None
-    }
+    val to = mailRecord.getTo
+    getAsScalaListOption(to)
   }
 
   def getCcOpt(): Option[List[String]] = {
-    val ccs = mailRecord.getCc
-    if (ccs != null) {
-      val scalaList = ccs.asScala.toList
-      Some(scalaList)
-    } else {
-      None
-    }
+    val cc = mailRecord.getCc
+    getAsScalaListOption(cc)
   }
 
   def getBccOpt(): Option[List[String]] = {
-    val bccs = mailRecord.getBcc
-    if (bccs != null) {
-      val scalaList = bccs.asScala.toList
-      Some(scalaList)
-    } else {
-      None
-    }
+    val bcc = mailRecord.getBcc
+    getAsScalaListOption(bcc)
   }
 
   def getMailFieldsAsScala(): Map[String, String] = {
     val mailFields = mailRecord.getMailFields
     val mutableMap = mailFields.asScala
     mutableMap.toMap
+  }
+
+  def getDateUtc(): DateTime = {
+    val dateUtcEpoch = mailRecord.getDateUtcEpoch
+    new DateTime(dateUtcEpoch, DateTimeZone.UTC)
+  }
+
+  /**
+   * Returns Some[scala.collection.immutable.List[T]] if javaList is not null,
+   * None otherwise (thanks, Jeff!)
+   */
+  private def getAsScalaListOption[T](javaList: java.util.List[T]): Option[scala.collection.immutable.List[T]] = {
+    Option(javaList).map {
+      java => java.asScala.toList
+    }
   }
 
 }
