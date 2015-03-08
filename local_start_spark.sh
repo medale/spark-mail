8516930e67d509a36d85f7c0b41cf2f4f7ce0f55
medale <medale94@yahoo.com>
2015-03-08 14:49:54 -0400

    Made spark,hadoop,avro depedencies provided.

---
+++ local_start_spark.sh
@@ -0,0 +1,4 @@
+spark-shell --master local[*] --driver-memory 2G --executor-memory 2G \
+--jars mailrecord-utils/target/mailrecord-utils-0.9.0-SNAPSHOT-shaded.jar \
+--properties-file mailrecord-utils/mailrecord.conf \
+--driver-java-options "-Dlog4j.configuration=log4j.properties"
